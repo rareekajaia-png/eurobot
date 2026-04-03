e9911049ae9955217674bbc6ddaa1c6c05bf1e26
rareekajaia-png <rareekajaia@gmail.com>
2026-04-03 21:38:24 +0300

    percent bets + fmt

--- main.py
+++ main.py
@@ -1534,7 +1534,7 @@ async def process_broadcast(msg: Message, state: FSMContext):
         try:
             await bot.send_message(
                 user_id,
-                f'</b>\n\n{broadcast_text}',
+                broadcast_text,
                 parse_mode="HTML"
             )
             success_count += 1
